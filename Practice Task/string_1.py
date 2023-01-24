9476157c8009f00ee2b6761ab3c0745f7fac3377
Anik <aniksen360@gmail.com>
2023-01-24 21:56:28 +0600

    updated

--- Practice Task/string_1.py
+++ Practice Task/string_1.py
@@ -5,3 +5,9 @@ print(name)
 print(name.title())
 print(name.upper())
 print(name.lower())
+"""
+• The title() method changes each word to title case, where each word
+begins with a capital letter.
+• The upper() and lower() methods change a string to all uppercase or
+all lowercase letters respectively.
+"""
