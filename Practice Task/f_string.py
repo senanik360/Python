f5c271ed7e834c26e54968a7ccc4aa657c895bcd
Anik <aniksen360@gmail.com>
2023-01-24 22:02:31 +0600

    tried

---
+++ Practice Task/f_string.py
@@ -0,0 +1,8 @@
+fname="Anik"
+lname='Sen'
+fullName=f"{fname} {lname}"
+print (fullName)
+'''
+Strings represent with f is called f-strings. The f is for format, because
+Python formats the string by replacing the name of any variable in
+braces with its value.'''
---
+++ Practice Task/print_1.py
@@ -0,0 +1,9 @@
+print('a', end='')
+print('b')
+"""
+• Note that print() always ends with an invisible "new line" character (
+\n ) so that repeated calls to print will all print on a separate line
+each.
+• To prevent this newline character from being printed, you can specify
+that it should end with a blank:
+"""
