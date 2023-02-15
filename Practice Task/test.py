e2372b85aa7c224d367101285a66c9f868e3147f
Anik <aniksen360@gmail.com>
2023-02-15 11:00:52 +0600

    tried

--- Practice Task/test.py
+++ Practice Task/test.py
@@ -1,8 +1,43 @@
-x=10
-print(type(x))
+name = ['anik', 'alok', 'anushree']
 
-a,b,c=1,2,3
-print(a)
-print(b)
-print(c)
+
+
+
+num = [1,2,3,4,5,6,7,8,9,10]
+
+print(min(num))
+print(max(num))
+print(sum(num))
+
+sq = [value**2 for value in range(1,11)]
+
+print(sq)
+players = ['a','b','a','b','a','b']
+
+player=players[:]
+
+print(player)
+player[0]='c'
+print(player)
+print(players)
+
+
+
+cars={'ab':'ba', 'bc':'cb'}
+
+for key,value in cars.items():
+    print(key+"  :  "+value)
+
+
+for i in set(players):
+    print(i.title())
+
+check = ['and', 'or', 'not']
+
+print(check[0])
+
+fname = 'anik'
+lname = 'sen'
+
+print("{} {}".format(fname,lname))
 
