011afff3b4a72dec3bb9d60bbc2c8c3f33963f21
Anik <aniksen360@gmail.com>
2023-03-31 13:03:54 +0600

    note added

--- Numpy/numpy.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-# np.__version__
-np.array([2,4,6,8,10])
---
+++ Numpy/prac1.py
@@ -0,0 +1,7 @@
+import numpy as np
+print(np.__version__)
+a = np.array([2,4,6,8,10])
+print(a)
+print(a.shape)
+print(a.ndim)
+type(a)
